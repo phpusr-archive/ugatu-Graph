e8e3f05e82c023efce1dfb153745549cf9c123c8
phpusr <phpusr@gmail.com>
2013-04-20 13:40:19 +0600

    Изменен внешний вид формы, убраны лишние компоненты

--- src/main/java/org/dyndns/phpusr/graph/GraphForm.java
+++ src/main/java/org/dyndns/phpusr/graph/GraphForm.java
@@ -1,17 +1,13 @@
 package org.dyndns.phpusr.graph;
 
-import com.mxgraph.examples.swing.editor.DefaultFileFilter;
 import com.mxgraph.util.mxResources;
 
 import javax.swing.*;
-import javax.swing.filechooser.FileFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.io.File;
-import java.io.IOException;
 
 /**
  * @author phpusr
@@ -24,13 +20,10 @@ public class GraphForm {
     private JButton btnAdd;
     private JPanel pnlMain;
     private JPanel pnlGraph;
-    private JButton btnEncode;
+    private JPanel pnlBtn;
     private JButton btnExit;
-    private JButton btnSave;
-    private JButton btnOpen;
     private JButton btnNew;
     private JButton btnDelete;
-    private JButton btnAbout;
     private JTextField txtHead;
     private GraphUtil util;
 
@@ -43,73 +36,9 @@ public class GraphForm {
                 util.addVertex();
             }
         });
-        btnEncode.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                util.getEncodeGraph();
-            }
-        });
         btnExit.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 util.exit();
-            }
-        });
-        btnSave.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    JFileChooser fc = new JFileChooser();
-                    fc.addChoosableFileFilter(new DefaultFileFilter(Const.EXT_DEF,
-                            Const.CHOOSE_FILE_FILTER_DESCRIP));
-                    int rc = fc.showDialog(null, mxResources.get("saveFile"));
-
-                    if (rc == JFileChooser.APPROVE_OPTION) {
-                        String filename = fc.getSelectedFile().getAbsolutePath();
-                        FileFilter selectedFilter = fc.getFileFilter();
-
-                        //Добавление расширения файлу, если нет
-                        if (selectedFilter instanceof DefaultFileFilter) {
-                            String ext = ((DefaultFileFilter) selectedFilter)
-                                    .getExtension();
-                            if (!filename.toLowerCase().endsWith(ext)) {
-                                filename += ext;
-                            }
-                        }
-
-                        //Проверка на существование файла
-                        if (new File(filename).exists()
-                                && JOptionPane.showConfirmDialog(util.getGraphComponent(),
-                                mxResources.get("overwriteExistingFile")) != JOptionPane.YES_OPTION) {
-                            return;
-                        }
-
-                        util.saveToFile(filename);
-                    }
-                } catch (Throwable ex) {
-                    ex.printStackTrace();
-                    JOptionPane.showMessageDialog(util.getGraphComponent(),
-                            ex.toString(), mxResources.get("error"),
-                            JOptionPane.ERROR_MESSAGE);
-                }
-            }
-        });
-        btnOpen.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser fc = new JFileChooser();
-                fc.addChoosableFileFilter(new DefaultFileFilter(Const.EXT_DEF,
-                        Const.CHOOSE_FILE_FILTER_DESCRIP));
-                int rc = fc.showDialog(null, mxResources.get("openFile"));
-                if (rc == JFileChooser.APPROVE_OPTION) {
-                    try {
-                        File file = fc.getSelectedFile();
-                        util.openFile(file);
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                        JOptionPane.showMessageDialog(
-                                util.getGraphComponent(),
-                                ex.toString(),
-                                mxResources.get("error"),
-                                JOptionPane.ERROR_MESSAGE);
-                    }
-                }
             }
         });
         btnNew.addActionListener(new ActionListener() {
@@ -125,16 +54,6 @@ public class GraphForm {
                 util.deleteCell();
             }
         });
-        btnAbout.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(util.getGraphComponent(),
-                        mxResources.get("about.description"),
-                        mxResources.get("about"),
-                        JOptionPane.QUESTION_MESSAGE,
-                        new ImageIcon(GraphForm.class.getResource("images/phpusr.png"))
-                );
-            }
-        });
         txtHead.addKeyListener(new KeyAdapter() {
             @Override
             public void keyReleased(KeyEvent e) {                
@@ -148,13 +67,9 @@ public class GraphForm {
      */
     private void localizeForm() {
         btnNew.setText(mxResources.get("new"));
-        btnOpen.setText(mxResources.get("openFile"));
-        btnSave.setText(mxResources.get("saveFile"));
         btnAdd.setText(mxResources.get("add"));
         btnDelete.setText(mxResources.get("delete"));
-        btnEncode.setText(mxResources.get("encode"));
         btnExit.setText(mxResources.get("exit"));
-        btnAbout.setText(mxResources.get("about"));
     }
 
     /**
